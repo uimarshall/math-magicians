5d409c660b1f616f965ab35eed6ae915da82362e
uimarshall <uimarshall@gmail.com>
2021-02-08 11:41:30 +0100

    Add ButtonPanel and Display components

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ButtonPanel() {
+  return (
+    <div>
+      <p>button</p>
+
+    </div>
+  );
+}
+
+export default ButtonPanel;
---
+++ src/components/Display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Display() {
+  return (
+    <div>
+      <p>display</p>
+    </div>
+  );
+}
+
+export default Display;

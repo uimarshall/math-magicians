0ab7e697752846673200622cd31092b80f0ec734
uimarshall <uimarshall@gmail.com>
2021-02-17 16:04:53 +0100

    cleanup code and complete the React routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-
 import '../App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar';
@@ -7,39 +5,6 @@ import Home from './Home';
 import Calculator from './Calculator';
 import Footer from './Footer';
 import Quote from './Quote';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       total: null,
-//       next: null,
-//       operation: null,
-//     };
-//   }
-
-//   handleClick = buttonName => {
-//     const calc = calculator.calculate(this.state, buttonName);
-//     this.setState(calc);
-//   }
-
-//   render() {
-//     const showResult = state => {
-//       const { total, next, operation } = state;
-//       const result = `${total}${operation}${next}`.replace(/null/g, '');
-//       return result === '' ? undefined : result;
-//     };
-//     return (
-//       <div className="App">
-
-//         <Display result={showResult(this.state)} />
-//         <ButtonPanel clickHandler={this.handleClick} />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
 
 const App = () => (
   <div className="App">

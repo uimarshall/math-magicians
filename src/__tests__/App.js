ce795fba15e47853e7e899486283b9dbfa71482b
uimarshall <uimarshall@gmail.com>
2021-02-18 23:49:43 +0100

    Add __snapshots__ to the routes testing in App.js

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,8 +1,11 @@
+/* eslint-disable import/no-duplicates */
 import '@testing-library/jest-dom';
 
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from '../components/App';
+import Link from '../components/App';
 
 describe('App', () => {
   test('renders App component', () => {
@@ -11,5 +14,26 @@ describe('App', () => {
     render(<App />);
 
     expect(screen.queryByText(nonExist)).toBeNull();
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Link to="/">Home</Link>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Link to="/calculator">Calculator</Link>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<Link to="/quote">Quote</Link>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
